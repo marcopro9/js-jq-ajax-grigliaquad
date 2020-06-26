2161df4acdb720997678749ee6cc921a43b97e3d
Marco <Prosperi>
2020-06-26 10:06:28 +0200

    modifica script

--- js/script.js
+++ js/script.js
@@ -13,17 +13,14 @@ $(document).ready(
         // In caso di successo, parte la funzione che prende i dati e...
         success: function (data){
           var numero = data.response;
-          var risultato = '';
           // ...se il numero casuale è minore o uguale a 5 il background sarà giallo...
           if (numero <= 5) {
-            risultato = numero;
             $(clickedDiv).text(numero);
             $(clickedDiv).addClass('yellow')
             // rimuovo la classe green in caso al click precedente fosse maggiore di 5 e il div verde
             $(clickedDiv).removeClass('green')
           } //...altrimenti se è maggiore di 5 sarà verde.
-            else if (numero > 5) {
-              risultato = numero;
+            else {
               $(clickedDiv).text(numero);
               $(clickedDiv).addClass('green')
               // rimuovo la classe yellow in caso al click precedente fosse minore o uguale a 5 e il div giallo
